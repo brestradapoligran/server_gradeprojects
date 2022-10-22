685d98a5879010efc3231d74a4e41536c3c6ba66
Brayan Estrada <brestrada@poligran.edu.co>
2022-10-22 15:05:32 -0500

    Feature: Create, update, getUser and getUsers are added

--- src/main/java/com/grade/project/infrastructure/config/bean/UserServiceBean.java
+++ src/main/java/com/grade/project/infrastructure/config/bean/UserServiceBean.java
@@ -2,6 +2,9 @@ package com.grade.project.infrastructure.config.bean;
 
 import com.grade.project.domain.port.UserRepository;
 import com.grade.project.domain.services.user.CreateUserService;
+import com.grade.project.domain.services.user.GetUserService;
+import com.grade.project.domain.services.user.GetUsersService;
+import com.grade.project.domain.services.user.UpdateUserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,5 +14,20 @@ public class UserServiceBean {
     @Bean
     public CreateUserService createUserService(UserRepository userRepository) {
         return new CreateUserService(userRepository);
+    }
+
+    @Bean
+    public UpdateUserService updateUserService(UserRepository userRepository) {
+        return new UpdateUserService(userRepository);
+    }
+
+    @Bean
+    public GetUserService getUserService(UserRepository userRepository) {
+        return new GetUserService(userRepository);
+    }
+
+    @Bean
+    public GetUsersService getUsersService(UserRepository userRepository) {
+        return new GetUsersService(userRepository);
     }
 }
---
+++ src/main/java/com/grade/project/infrastructure/controllers/user/UpdateUserController.java
@@ -0,0 +1,21 @@
+package com.grade.project.infrastructure.controllers.user;
+
+import com.grade.project.application.command.UserCommand;
+import com.grade.project.application.handler.user.UpdateUserHandler;
+import com.grade.project.domain.dto.UserDto;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@AllArgsConstructor
+public class UpdateUserController {
+
+    private final UpdateUserHandler updateUserHandler;
+
+    @PutMapping("/{id}")
+    public UserDto updateUser(@RequestBody UserCommand userCommand, @PathVariable String id) {
+        userCommand.setId(id);
+        return this.updateUserHandler.updateUser(userCommand);
+    }
+}
