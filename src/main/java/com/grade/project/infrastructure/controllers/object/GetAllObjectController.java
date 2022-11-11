a952cdc96c9be254990d485f83be12955bdb057a
brestradapoligran <brestrada@poligran.edu.co>
2022-11-10 22:13:21 -0500

    Feature: GetAllObjectController updated

--- src/main/java/com/grade/project/infrastructure/controllers/object/GetAllObjectController.java
+++ src/main/java/com/grade/project/infrastructure/controllers/object/GetAllObjectController.java
@@ -3,6 +3,7 @@ package com.grade.project.infrastructure.controllers.object;
 import com.grade.project.application.handler.object.GetAllObjectsHandler;
 import com.grade.project.domain.dto.ObjectDto;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class GetAllObjectController {
     private final GetAllObjectsHandler getAllObjectsHandler;
 
     @GetMapping
+    @CrossOrigin(origins = "http://localhost:4200")
     public List<ObjectDto> getObjects() {
         return this.getAllObjectsHandler.getObjects();
     }
