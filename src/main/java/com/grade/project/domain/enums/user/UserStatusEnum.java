113806272f303c79a22f7f653ab0351571b7e16d
brestradapoligran <brestrada@poligran.edu.co>
2022-12-26 19:08:12 -0500

    feature/fixes

---
+++ src/main/java/com/grade/project/domain/enums/user/UserStatusEnum.java
@@ -0,0 +1,5 @@
+package com.grade.project.domain.enums.user;
+
+public enum UserStatusEnum {
+    Activo, Inactivo
+}
--- src/main/java/com/grade/project/infrastructure/document/UserDocument.java
+++ src/main/java/com/grade/project/infrastructure/document/UserDocument.java
@@ -1,6 +1,7 @@
 package com.grade.project.infrastructure.document;
 
 import com.grade.project.domain.enums.user.UserRoleEnum;
+import com.grade.project.domain.enums.user.UserStatusEnum;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,5 +20,5 @@ public class UserDocument {
     private String email;
     private String pass;
     private UserRoleEnum role;
-    private Boolean status;
+    private UserStatusEnum status;
 }
